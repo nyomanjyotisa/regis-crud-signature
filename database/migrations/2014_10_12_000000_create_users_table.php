6d670e2daa128c7262627a6c6da8f6099e2009a3
nyomanjyotisa <jyotisa1616@gmail.com>
2022-07-01 13:06:12 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['super-admin', 'admin'])->default('admin');
+            $table->ememberToken();
             $table->timestamps();
         });
     }
